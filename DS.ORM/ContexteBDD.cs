9af7f56e54db1bc074a44c5f2a530adb316edc2c
Flavyen Tonnelier <flavyen.tonnelier@epsi.fr>
2019-11-14 11:24:35 +0100

    Ajoutez des fichiers projet.

---
+++ DS.ORM/ContexteBDD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.ORM
+{
+    public class ContexteBDD : DbContext
+    {
+        public ContexteBDD()
+            : base("chaineDeConnexion")
+        {
+
+        }
+        //public DbSet <Categorie> Categories { get; set; }
+        //public DbSet<Chaton> Chatons { get; set; }
+
+        //public DbSet<Proprio> Proprios { get; set; }
+
+    }
+}
---
+++ DS.ORM/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DS.ORM.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DS.ORM.ContexteBDD>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DS.ORM.ContexteBDD context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
